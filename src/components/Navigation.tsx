53f4e41bb94d39359ec82b35594885e98c6d3ce7
MOHAMMAD ABUZAR <mohammadabuzar316@gmail.com>
2023-11-15 20:39:56 +0000

    create navigation bar

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+export default function Navigation() {
+  return (
+    <header className="md:container">
+      <nav className="flex justify-between">
+        <div>
+          <h2>Mohammad Abuzar</h2>
+        </div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
