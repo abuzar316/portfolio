c5037665a0e125344b7cbc11f9084ceec61dfced
MOHAMMAD ABUZAR <mohammadabuzar316@gmail.com>
2023-11-16 19:56:43 +0000

    navigation social icon added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,6 @@
+import { GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+
 
 export default function Navigation() {
   return (
@@ -16,7 +18,7 @@ export default function Navigation() {
               <li>
                 <Link
                   href={"#"}
-                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                  className="inline-block py-3 text-lg ease-in-out duration-300 px-2 hover:text-secondary cursor-pointer"
                 >
                   Home
                 </Link>
@@ -24,7 +26,7 @@ export default function Navigation() {
               <li>
                 <Link
                   href={"#"}
-                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                  className="inline-block py-3 text-lg ease-in-out duration-300 px-2 hover:text-secondary cursor-pointer"
                 >
                   About
                 </Link>
@@ -32,12 +34,24 @@ export default function Navigation() {
               <li>
                 <Link
                   href={"#"}
-                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                  className="inline-block py-3 text-lg ease-in-out duration-300 px-2 hover:text-secondary cursor-pointer"
                 >
                   Contact
                 </Link>
               </li>
-              <li className="border-s-2 h-6">d</li>
+              <li className="flex items-center">
+                <div className="border-s-2 px-2 flex">
+                  <span className="cursor-pointer p-2">
+                    <GitHubLogoIcon className="ease-in-out duration-500 hover:text-secondary hover:scale-150" />
+                  </span>
+                  <span className="cursor-pointer p-2">
+                    <LinkedInLogoIcon className="ease-in-out duration-500 hover:text-secondary hover:scale-150" />
+                  </span>
+                  <span className="cursor-pointer p-2">
+                    <InstagramLogoIcon className="ease-in-out duration-500 hover:text-secondary hover:scale-150" />
+                  </span>
+                </div>
+              </li>
             </ul>
           </div>
         </nav>
