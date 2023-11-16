2bde49bcc36c20ab9c858018a326d95473dedc4b
MOHAMMAD ABUZAR <mohammadabuzar316@gmail.com>
2023-11-16 13:58:01 +0000

    craeting navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,47 @@
+import Link from "next/link";
+
 export default function Navigation() {
   return (
-    <header className="md:container">
-      <nav className="flex justify-between">
-        <div>
-          <h2>Mohammad Abuzar</h2>
-        </div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+    <div className="bg-primary text-white">
+      <header className="md:container ">
+        <nav className="flex justify-between items-center">
+          <div>
+            <h2 className="text-2xl">
+              <span className="text-secondary">M</span>ohammad{" "}
+              <span className="text-secondary">A</span>buzar
+            </h2>
+          </div>
+          <div>
+            <ul className="flex">
+              <li>
+                <Link
+                  href={"#"}
+                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"#"}
+                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"#"}
+                  className="inline-block py-3 px-2 hover:text-secondary cursor-pointer"
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="border-s-2 h-6">d</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#243c5a",
+        secondary: "#01d293",
+      },
     },
     container: {
       center: true,
